2f28bbf16be60d9e1625caed1883b137855ad37b
hamza-m-masood <hamzamasood183@gmail.com>
2026-06-18 13:43:48 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -16,5 +16,5 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Error reading input", err)
 		os.Exit(1)
 	}
-	fmt.Println(command[:len(command)-1], ": command not found")
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
