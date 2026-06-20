f6b805e066fc419bdb2875723093cb227091e5f8
hamza-m-masood <hamzamasood183@gmail.com>
2026-06-20 15:16:42 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
-	"io/fs"
 	"os"
-	"path/filepath"
+	"os/exec"
 	"slices"
 	"strings"
 )
@@ -47,37 +45,43 @@ func main() {
 				if slices.Contains(builtin, tokens[i]) {
 					fmt.Printf("%v is a shell builtin\n", tokens[i])
 				} else {
-					fileFound := false
-					pathDirs := strings.Split(os.Getenv("PATH"), ":")
-					for _, dir := range pathDirs {
-						files, err := os.ReadDir(dir)
-						if err != nil {
-							if errors.Is(err, fs.ErrNotExist) {
-								continue
-							}
-							fmt.Printf("error reading path %v: %v", dir, err)
-						}
-						for _, file := range files {
-							fullPath := filepath.Join(dir, file.Name())
-							fileInfo, err := os.Lstat(fullPath)
-							if err != nil {
-								fmt.Printf("error getting file info %v: %v", fullPath, err)
-							}
-							if !fileInfo.IsDir() {
-								permission := fileInfo.Mode().Perm()
-								if permission&0o111 != 0 && file.Name() == tokens[i] {
-									fmt.Printf("%v is %v\n", tokens[i], fullPath)
-									fileFound = true
-									break
-								}
-							}
-						}
-						if fileFound {
-							break
-						}
-					}
-					if !fileFound {
-						fmt.Println(tokens[i] + ": not found")
+					// fileFound := false
+					// pathDirs := strings.Split(os.Getenv("PATH"), ":")
+					// for _, dir := range pathDirs {
+					// 	files, err := os.ReadDir(dir)
+					// 	if err != nil {
+					// 		if errors.Is(err, fs.ErrNotExist) {
+					// 			continue
+					// 		}
+					// 		fmt.Printf("error reading path %v: %v", dir, err)
+					// 	}
+					// 	for _, file := range files {
+					// 		fullPath := filepath.Join(dir, file.Name())
+					// 		fileInfo, err := os.Lstat(fullPath)
+					// 		if err != nil {
+					// 			fmt.Printf("error getting file info %v: %v", fullPath, err)
+					// 		}
+					// 		if !fileInfo.IsDir() {
+					// 			permission := fileInfo.Mode().Perm()
+					// 			if permission&0o111 != 0 && file.Name() == tokens[i] {
+					// 				fmt.Printf("%v is %v\n", tokens[i], fullPath)
+					// 				fileFound = true
+					// 				break
+					// 			}
+					// 		}
+					// 	}
+					// 	if fileFound {
+					// 		break
+					// 	}
+					// }
+					// if !fileFound {
+					// 	fmt.Println(tokens[i] + ": not found")
+					// }
+					path, err := exec.LookPath(tokens[i])
+					if err != nil {
+						fmt.Println(tokens[i] + ": command not found")
+					} else {
+						fmt.Printf("%v is %v\n", tokens[i], path)
 					}
 				}
 			}
