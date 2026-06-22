5a9dcb5f92acb90b9a953ee6663a1dd8378550a5
hamza-m-masood <hamzamasood183@gmail.com>
2026-06-22 14:21:44 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -58,7 +58,84 @@ func tokenize(command string) []string {
 	return tokens
 }
 
-var _ = fmt.Print
+func processTokens(tokens []string) (string, error) {
+	builtin := []string{"exit", "echo", "type", "pwd", "cd"}
+	switch tokens[0] {
+	case "echo":
+		echoOutput := fmt.Sprintln(strings.Join(tokens[1:], " "))
+		return echoOutput, nil
+	case "cat":
+		files := tokens[1:]
+		content := []string{}
+		for _, file := range files {
+			contentBytes, err := os.ReadFile(file)
+			if err != nil {
+				fmt.Printf("error reading file: %v: %v\n", file, err)
+			}
+			content = append(content, strings.TrimSpace(string(contentBytes)))
+		}
+		return strings.Join(content, ""), nil
+	case "pwd":
+		wd, err := os.Getwd()
+		if err != nil {
+			return "", err
+		}
+		return wd, nil
+	case "type":
+		for i := 1; i < len(tokens); i++ {
+			if slices.Contains(builtin, tokens[i]) {
+				return fmt.Sprintf("%v is a shell builtin\n", tokens[i]), nil
+			} else {
+				path, err := exec.LookPath(tokens[i])
+				if err != nil {
+					typeErrorOutput := fmt.Sprint(tokens[i] + ": not found")
+					typeError := errors.New(typeErrorOutput)
+					return "", typeError
+				} else {
+					return fmt.Sprintf("%v is %v\n", tokens[i], path), nil
+				}
+			}
+		}
+	case "cd":
+		var err error
+		if tokens[1] == "~" {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				cdErrorOutput := fmt.Sprintf("error geting home directory of user: %v", err)
+				cdError := errors.New(cdErrorOutput)
+				return "", cdError
+			}
+			err = os.Chdir(homeDir)
+		} else {
+			err = os.Chdir(tokens[1])
+		}
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				cdErrorOutput := fmt.Sprintf("cd: %v: No such file or directory\n", tokens[1])
+				cdError := errors.New(cdErrorOutput)
+				return "", cdError
+			} else {
+				return fmt.Sprintf("error changing directory: %v\n", err), nil
+			}
+		}
+	default:
+		_, err := exec.LookPath(tokens[0])
+		if err != nil {
+			return fmt.Sprintln(tokens[0] + ": not found"), nil
+		} else {
+			cmd := exec.Command(tokens[0], tokens[1:]...)
+			// cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			err := cmd.Run()
+			if err != nil {
+				defaultErrorOutput := fmt.Sprintf("couldn't run command: %v: %v", tokens[0], err)
+				defaultError := errors.New(defaultErrorOutput)
+				return "", defaultError
+			}
+		}
+	}
+	return "", nil
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -75,77 +152,56 @@ func main() {
 		}
 		command = strings.TrimSpace(command)
 		tokens := tokenize(command)
-		builtin := []string{"exit", "echo", "type", "pwd", "cd"}
-		switch tokens[0] {
-		case "exit":
+		if tokens[0] == "exit" {
 			if len(tokens) > 1 {
 				fmt.Fprintln(os.Stderr, "exit: too many arguments")
 				continue
 			}
 			return
-		case "echo":
-			fmt.Println(strings.Join(tokens[1:], " "))
-		case "cat":
-			files := tokens[1:]
-			content := []string{}
-			for _, file := range files {
-				contentBytes, err := os.ReadFile(file)
-				if err != nil {
-					fmt.Printf("error reading file: %v: %v\n", file, err)
-				}
-				content = append(content, strings.TrimSpace(string(contentBytes)))
-			}
-			fmt.Println(strings.Join(content, ""))
-		case "pwd":
-			wd, err := os.Getwd()
-			if err != nil {
-				fmt.Println("Couldn't get current working directory: %v", err)
-			}
-			fmt.Printf("%v\n", wd)
-		case "type":
-			for i := 1; i < len(tokens); i++ {
-				if slices.Contains(builtin, tokens[i]) {
-					fmt.Printf("%v is a shell builtin\n", tokens[i])
-				} else {
-					path, err := exec.LookPath(tokens[i])
-					if err != nil {
-						fmt.Println(tokens[i] + ": not found")
-					} else {
-						fmt.Printf("%v is %v\n", tokens[i], path)
-					}
-				}
-			}
-		case "cd":
-			var err error
-			if tokens[1] == "~" {
-				homeDir, err := os.UserHomeDir()
-				if err != nil {
-					fmt.Printf("error geting home directory of user: %v", err)
-				}
-				err = os.Chdir(homeDir)
-			} else {
-				err = os.Chdir(tokens[1])
-			}
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
-					fmt.Printf("cd: %v: No such file or directory\n", tokens[1])
-				} else {
-					fmt.Println("error changing directory: %v\n", err)
-				}
-			}
-		default:
-			_, err := exec.LookPath(tokens[0])
-			if err != nil {
-				fmt.Println(tokens[0] + ": not found")
-			} else {
-				cmd := exec.Command(tokens[0], tokens[1:]...)
-				// cmd.Stdin = os.Stdin
-				cmd.Stdout = os.Stdout
-				err := cmd.Run()
-				if err != nil {
-					fmt.Printf("couldn't run command: %v: %v", tokens[0], err)
-				}
-			}
 		}
+		if slices.Contains(tokens, ">") || slices.Contains(tokens, "1>") {
+			var index int
+			if slices.Contains(tokens, ">") {
+				index = slices.Index(tokens, ">")
+			} else if slices.Contains(tokens, "1>") {
+				index = slices.Index(tokens, "1>")
+			}
+
+			if index == -1 {
+				fmt.Println("Could not redirect output")
+			}
+
+			p := tokens[:index]
+			output, err := processTokens(p)
+			if err != nil {
+				fmt.Print(err)
+			}
+			//only one file on right side of ">" operator
+			if len(tokens[index+1:]) != 1 {
+				fmt.Println("Please enter only one file name")
+				continue
+
+			}
+			fileName := tokens[index+1:][0]
+			// check for existence
+			if _, err := os.Stat(fileName); err != nil {
+				fmt.Printf("%v: nonexistent: No such file or directory\n", tokens[0])
+				continue
+			}
+			b := []byte(output)
+			err = os.WriteFile(fileName, b, 0644)
+			if err != nil {
+				fmt.Println("error writing to file:", err)
+			}
+		} else {
+			output, err := processTokens(tokens)
+			if err != nil {
+				fmt.Println(err)
+			}
+			//TODO: fix fmt.Print to fmt.Println
+			fmt.Print(output)
+		}
+
 	}
+
 }
